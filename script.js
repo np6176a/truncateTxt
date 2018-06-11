791dbd67186b4c4d5d6858af994a6e30c07c316b
Niya Panamdanam <niya@findniya.com>
2018-06-11 19:30:47 -0400

    inital test with window resize

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-$(window).on('load', function (){
+// the ellipses
+$(window).on('load resize', function ellipses () {
+  var container = $('.box-content');
+  var containerHeight = 200;
+  $(container).each(function () {
+    var wrap = $(this).find('.content');
+    var para = $(this).find('p');
+    var heightDifference = $(this).outerHeight() - containerHeight;
+    var wrapHeight = wrap.height() - heightDifference;
+    var paraLineHeight = parseInt($(this).find('h4').css('line-height')) || 20;
 
-    var container = $('.container');
-    var maxContainerHeight = 250
-    window.ellipses(container,maxContainerHeight);
+    $(wrap).css('height', wrapHeight);
+    if (wrapHeight >= paraLineHeight) {
+      while (para.outerHeight() > wrap.height()) {
+        if (!$(para).text().match(/\W*\s(\S)*$/)) {
+          break
+        }
+        else {
+          $(para).text(function (index, text) {
+            return text.replace(/\W*\s(\S)*$/, '[...]');
+          })
+        }
+        ;
+      }
+    }
+    else {
+      $(wrap).hide();
+    }
 
-
+  });
 });
