3dde215e74982d2658404dcdf7b2d7b93620d695
Niya Panamdanam <niya@findniya.com>
2018-06-11 23:09:48 -0400

    adjusted script to re-enter text when window resizes

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 // the ellipses
-$(window).on('load resize', function ellipses () {
+function ellipsesInit() {
   var container = $('.box-content');
   var containerHeight = 200;
   $(container).each(function () {
     var wrap = $(this).find('.content');
     var para = $(this).find('p');
+    var text = $(para).text();
+    var data = $(para).data("content", text);
+
+    $(para).attr("original", data[0].innerText);
+
     var heightDifference = $(this).outerHeight() - containerHeight;
     var wrapHeight = wrap.height() - heightDifference;
-    var paraLineHeight = parseInt($(this).find('h4').css('line-height')) || 20;
-
+    var paraLineHeight = parseInt($(this).find('p').css('line-height')) || 20;
     $(wrap).css('height', wrapHeight);
+
     if (wrapHeight >= paraLineHeight) {
-      while (para.outerHeight() > wrap.height()) {
+      while (para.outerHeight() > wrapHeight) {
         if (!$(para).text().match(/\W*\s(\S)*$/)) {
           break
         }
@@ -26,6 +31,40 @@ $(window).on('load resize', function ellipses () {
     else {
       $(wrap).hide();
     }
+  });
+}
 
+
+// onload
+$(window).on('load', ellipsesInit());
+
+// onresize
+$(window).on('resize', function ellipses() {
+  var container = $('.box-content');
+  var containerHeight = 200;
+  $(container).each(function () {
+    var wrap = $(this).find('.content');
+    var para = $(this).find('p');
+    var heightDifference = $(this).outerHeight() - containerHeight;
+    var wrapHeight = wrap.height() - heightDifference;
+    var paraLineHeight = parseInt($(this).find('p').css('line-height')) || 20;
+    $(wrap).css('height', wrapHeight);
+    if (wrapHeight >= paraLineHeight) {
+      $(para).text($(para).attr('original'));
+      while (para.outerHeight() > wrapHeight) {
+        if (!$(para).text().match(/\W*\s(\S)*$/)) {
+          break
+        }
+        else {
+          $(para).text(function (index, text) {
+            return text.replace(/\W*\s(\S)*$/, '[...]');
+          })
+        }
+        ;
+      }
+    }
+    else {
+      $(wrap).hide();
+    }
   });
 });
